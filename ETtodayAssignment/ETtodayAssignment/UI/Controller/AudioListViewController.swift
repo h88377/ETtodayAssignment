bdac66b76056caeb9f3f6dcc67d24db5ee434adf
Wayne <h88377@gmail.com>
2023-05-17 17:45:02 +0800

    Move `AudioListUIComposer` to its own file

--- ETtodayAssignment/ETtodayAssignment/UI/Controller/AudioListViewController.swift
+++ ETtodayAssignment/ETtodayAssignment/UI/Controller/AudioListViewController.swift
@@ -6,33 +6,6 @@
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
-
-final class AudioListUIComposer {
-    private init() {}
-    private static let disposeBag = DisposeBag()
-    
-    static func AudioUIComposedWith(audioLoader: AudioLoader) -> AudioListViewController {
-        let viewModel = AudioListViewModel(audioLoader: audioLoader)
-        let controller = AudioListViewController(viewModel: viewModel)
-        
-        viewModel.audios
-            .subscribe(onNext: { [weak controller] audios in
-                let cellControllers = audios.map { AudioListCellViewController(audio: $0) }
-                controller?.set(audios: cellControllers)
-                controller?.collectionView.isHidden = audios.isEmpty
-            }).disposed(by: disposeBag)
-        
-        viewModel.reminder
-            .subscribe(onNext: { [weak controller] reminder in
-                controller?.setReminder(reminder)
-                controller?.collectionView.isHidden = true
-            }).disposed(by: disposeBag)
-        
-        return controller
-    }
-}
 
 final class AudioListViewController: UICollectionViewController {
     let searchBar = UISearchBar()
