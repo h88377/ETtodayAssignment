e867c57e93d4dde2164685c4fd1d7bec611ca82d
Wayne <h88377@gmail.com>
2023-05-17 14:12:45 +0800

    `AudioListViewController` does not request audio upon creation

--- ETtodayAssignment/ETtodayAssignmentTests/AudioListViewControllerTests.swift
+++ ETtodayAssignment/ETtodayAssignmentTests/AudioListViewControllerTests.swift
@@ -6,7 +6,44 @@
 //
 
 import XCTest
+@testable import ETtodayAssignment
+
+class AudioListViewController: UIViewController {
+    private let audioLoader: AudioLoader
+    
+    init(audioLoader: AudioLoader) {
+        self.audioLoader = audioLoader
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
 
 final class AudioListViewControllerTests: XCTestCase {
-
+    
+    func test_init_doesNotRequestAudioes() {
+        let loader = AudioLoaderSpy()
+        _ = AudioListViewController(audioLoader: loader)
+        
+        XCTAssertEqual(loader.receivedKeywords, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private class AudioLoaderSpy: AudioLoader {
+        private(set) var receivedKeywords = [String]()
+        
+        func loadAudio(with keyword: String) {
+            receivedKeywords.append(keyword)
+        }
+    }
 }
