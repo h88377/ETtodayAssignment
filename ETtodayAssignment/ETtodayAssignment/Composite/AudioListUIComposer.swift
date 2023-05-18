907d29edb78a047250c9bb68c99a8eadc717125e
Wayne <h88377@gmail.com>
2023-05-18 19:18:03 +0800

    Move `MainThreadDispatchDecorator` to its own file

--- ETtodayAssignment/ETtodayAssignment/Composite/AudioListUIComposer.swift
+++ ETtodayAssignment/ETtodayAssignment/Composite/AudioListUIComposer.swift
@@ -35,35 +35,3 @@ final class AudioListUIComposer {
         return controller
     }
 }
-
-final class MainThreadDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { completion() }
-        }
-        
-        completion()
-    }
-}
-
-extension MainThreadDispatchDecorator: AudioLoader where T == AudioLoader {
-    func loadAudio(with keyword: String, completion: @escaping (AudioLoader.Result) -> Void) {
-        decoratee.loadAudio(with: keyword) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
-extension MainThreadDispatchDecorator: AudioImageDataLoader where T == AudioImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (AudioImageDataLoader.Result) -> Void) {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
---
+++ ETtodayAssignment/ETtodayAssignment/Composite/MainThreadDispatchDecorator.swift
@@ -0,0 +1,40 @@
+//
+//  MainThreadDispatchDecorator.swift
+//  ETtodayAssignment
+//
+//  Created by 鄭昭韋 on 2023/5/18.
+//
+
+import Foundation
+
+final class MainThreadDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { completion() }
+        }
+        
+        completion()
+    }
+}
+
+extension MainThreadDispatchDecorator: AudioLoader where T == AudioLoader {
+    func loadAudio(with keyword: String, completion: @escaping (AudioLoader.Result) -> Void) {
+        decoratee.loadAudio(with: keyword) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+extension MainThreadDispatchDecorator: AudioImageDataLoader where T == AudioImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (AudioImageDataLoader.Result) -> Void) {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
