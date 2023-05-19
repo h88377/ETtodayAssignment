85a8245e168d92ce3ed1d4c29a500575a9fef502
Wayne <h88377@gmail.com>
2023-05-19 15:06:42 +0800

    Extract collection view configuration logic into a private method in `AudioListViewController`

--- ETtodayAssignment/ETtodayAssignment/UI/Controller/AudioListViewController.swift
+++ ETtodayAssignment/ETtodayAssignment/UI/Controller/AudioListViewController.swift
@@ -29,6 +29,13 @@ final class AudioListViewController: UIViewController {
         return collectionView
     }()
     
+    private var audioSection: Int { return 0 }
+    private lazy var dataSource: UICollectionViewDiffableDataSource<Int, AudioListCellViewController> = {
+        .init(collectionView: collectionView) { [weak self] collectionView, indexPath, controller in
+            return controller.view(in: collectionView, indexPath: indexPath)
+        }
+    }()
+    
     private var selectedCellController: AudioListCellViewController?
     private let viewModel: AudioListViewModel
     
@@ -36,13 +43,6 @@ final class AudioListViewController: UIViewController {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
-    private var audioSection: Int { return 0 }
-    private lazy var dataSource: UICollectionViewDiffableDataSource<Int, AudioListCellViewController> = {
-        .init(collectionView: collectionView) { [weak self] collectionView, indexPath, controller in
-            return controller.view(in: collectionView, indexPath: indexPath)
-        }
-    }()
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -54,10 +54,7 @@ final class AudioListViewController: UIViewController {
         setUpUI()
         view.backgroundColor = .white
         searchBar.delegate = self
-        collectionView.delegate = self
-        collectionView.dataSource = dataSource
-        collectionView.collectionViewLayout = configureCollectionViewLayout()
-        collectionView.register(AudioListCell.self, forCellWithReuseIdentifier: AudioListCell.identifier)
+        configureCollectionView()
         configureDismissKeyboard()
     }
     
@@ -89,6 +86,13 @@ final class AudioListViewController: UIViewController {
         reminder.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
+    }
+    
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = dataSource
+        collectionView.collectionViewLayout = configureCollectionViewLayout()
+        collectionView.register(AudioListCell.self, forCellWithReuseIdentifier: AudioListCell.identifier)
     }
     
     private func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
